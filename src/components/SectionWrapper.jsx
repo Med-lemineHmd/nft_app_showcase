ec17d6e985b87fee3a4e848d25e224c387c33a34
Mohamed lamine HAMDINOU <mohamedlaminehamdinou@Mohameds-MacBook-Pro.local>
2022-11-14 14:00:53 +0100

    Create Final showcase

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -42,7 +42,7 @@ const SectionWrapper = ({
             {description}
           </p>
           {showBtn && (
-            <Button assetUrl={assets.expo} link="deployed nft marketplace rn" />
+            <Button assetUrl={assets.expo} link="expo.dev/@med_lemine/react_native_nft?serviceType=classic&" />
           )}
         </div>
         <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>

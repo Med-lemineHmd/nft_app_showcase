21ab957a3b2c689ca6b13cfacb59708c35128f4c
Mohamed lamine HAMDINOU <mohamedlaminehamdinou@Mohameds-MacBook-Pro.local>
2022-11-14 04:11:54 +0100

    Project Structure

---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SectionWrapper = () => {
+  return <div>SectionWrapper</div>;
+};
+
+export default SectionWrapper;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Download } from "./Download";
+export { default as Features } from "./Features";
+export { default as SectionWrapper } from "./SectionWrapper";
